82944881435ed51282ec051bcfa27a7b00754253
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2020-12-11 19:39:04 +0100

    Add files via upload

---
+++ mo_lifeinvaderR/mo_lifeinvaderR_de.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES
+	('usbstick', 'USB Stick (leer)', '-1', '0', '1', '0');
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES
+	('usbstick_data', 'USB Stick (mit Daten)', '-1', '0', '1', '0');
+	
+INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES 
+	(NULL, 'TwentyFourSeven', 'usbstick', '20');
+
+INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES 
+	(NULL, 'LTDgasoline', 'usbstick', '25');
---
+++ mo_lifeinvaderR/mo_lifeinvaderR_en.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES
+	('usbstick', 'USB Stick (empty)', '-1', '0', '1', '0');
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES
+	('usbstick_data', 'USB Stick (with Data)', '-1', '0', '1', '0');
+	
+INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES 
+	(NULL, 'TwentyFourSeven', 'usbstick', '20');
+
+INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES 
+	(NULL, 'LTDgasoline', 'usbstick', '25');
