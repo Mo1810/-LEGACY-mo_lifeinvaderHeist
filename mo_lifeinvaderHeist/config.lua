90849f36ae211e8df0c17ee51cb20066e4ad45fe
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2021-02-12 07:47:58 +0100

    Renamed Script

---
+++ mo_lifeinvaderHeist/config.lua
@@ -0,0 +1,39 @@
+Config = {}
+Config.Locale = 'de'
+
+Config.greySquare = false --default: false
+Config.sellPrice = 65000 --($) default: 65000
+Config.DownloadWaitTime = 2 --(min) default: 2
+Config.NextRobberyWaitTime = 30 --(min) default: 30
+Config.UploadWaitTime = 1.5 --(min) default: 1.5
+Config.shutPowerDown = true --default: true
+Config.trigger_key = 46 --E key
+Config.RequiredPolice = 3 --default: 3
+
+Config.RobberyStandUp = {
+	Coords = {x = -1053.67, y = -230.3, z = 44.02},
+	Heading = 117.37,
+}
+Config.RobberyStart = {
+	Coords = {x = -1052.85, y = -230.89, z = 44.10},
+}
+Config.RobberyUSBStick = {
+	Coords = {x = -1051.89, y = -231.71, z = 44.02},
+	Heading = 23.42,
+}
+Config.LesterCoords = {
+	Coords = {x = 707.30, y = -967.55, z = 30.51},
+}
+Config.LBlip = {
+	Coords = {x = -1068.89, y = -247.2, z = 39.73},
+	Sprite = 77,
+	Display = 6,
+	Colour = 4,
+	Scale = 0.8,
+}
+Config.HackingBlip = {
+	Coords = {x = -1052.85, y = -230.89, z = 44.10},
+	Sprite = 521,
+	Display = 9,
+	Scale = 0.6,
+}
