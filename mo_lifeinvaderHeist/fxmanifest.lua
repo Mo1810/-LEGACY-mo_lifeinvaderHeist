eed25dbc3742026133a9f9f51e965c2866210c37
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2021-05-06 10:05:49 +0200

    Add files via upload

---
+++ mo_lifeinvaderHeist/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'bodacious'
+game 'gta5' 
+
+author 'Mo1810#4230'
+version '1.0.0'
+
+server_scripts {
+	"@mysql-async/lib/MySQL.lua",
+	"@es_extended/locale.lua",
+	"lifeinvaderHeist-s.lua",
+	"config.lua",
+	"locales/de.lua",
+	"locales/en.lua"
+}
+
+client_scripts {
+	"@es_extended/locale.lua",
+	"lifeinvaderHeist-c.lua",
+	"config.lua",
+	"locales/de.lua",
+	"locales/en.lua"
+}
+
+dependencies {
+	'es_extended'
+}
