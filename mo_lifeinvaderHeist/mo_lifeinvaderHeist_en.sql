77c312d13bb199aeb4bc5bebe4cecccaec0b6f3d
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2022-01-03 16:47:28 +0100

    add option for server with weight system

--- mo_lifeinvaderHeist/mo_lifeinvaderHeist_en.sql
+++ mo_lifeinvaderHeist/mo_lifeinvaderHeist_en.sql
@@ -1,3 +1,5 @@
+--ONLY NEED ONE OF THEM!
+--OPTION 1 IF YOU HAVE A LIMIT SYSTEM ON YOUR SERVER
 ﻿INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`, `price`) VALUES
 	('usbstick', 'USB Stick (empty)', '-1', '0', '1', '0');
 
@@ -9,3 +11,16 @@ INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES
 
 INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES 
 	(NULL, 'LTDgasoline', 'usbstick', '25');
+	
+--OPTION 2 IF YOU HAVE A WEIGHT SYSTEM ON YOUR SERVER
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+    ('usbstick', 'USB Stick (empty)', '0', '0', '1');
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+    ('usbstick_data', 'USB Stick (with Data)', '0', '0', '1');
+    
+INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES 
+    (NULL, 'TwentyFourSeven', 'usbstick', '20');
+
+INSERT INTO `shops` (`id`, `store`, `item`, `price`) VALUES 
+    (NULL, 'LTDgasoline', 'usbstick', '25');
