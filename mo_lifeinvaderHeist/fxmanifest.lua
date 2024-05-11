246f5a4996eb936ca72de0095051bad47ff8ff3a
Mo1810 <68502762+Mo1810@users.noreply.github.com>
2024-05-11 02:26:45 +0200

    Update fxmanifest.lua

--- mo_lifeinvaderHeist/fxmanifest.lua
+++ mo_lifeinvaderHeist/fxmanifest.lua
@@ -6,23 +6,18 @@ version '1.2.1'
 
 server_scripts {
 	"@mysql-async/lib/MySQL.lua",
-	"@es_extended/locale.lua",
 	"lifeinvaderHeist-s.lua",
-	"config.lua",
-	"locales/de.lua",
-	"locales/en.lua"
 }
 
 client_scripts {
-	"@es_extended/locale.lua",
 	"lifeinvaderHeist-c.lua",
-	"config.lua",
-	"locales/de.lua",
-	"locales/en.lua"
 }
 
 shared_scripts {
-	"@es_extended/imports.lua"
+ "@es_extended/locale.lua",
+	"@es_extended/imports.lua",
+ "config.lua",
+ "locales/*.lua"
 }
 
 dependencies {
